bd875b5803319bbd103bcd1ace0cd15da4840366
jcyenyen <amanda7776@gmail.com>
2023-09-07 21:08:34 +0800

    完成Table元件

---
+++ src/views/uBikeTable/components/pagination.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav v-if="pagerEnd > 0">
+    <ul class="pagination">
+      <li @click.prevent="setPage(currentPage - 1)" class="page-item">
+        <a class="page-link" href>Previous</a>
+      </li>
+
+      <li
+        v-for="i in pagerEnd"
+        :class="{ active: i + pagerAddAmount === currentPage }"
+        :key="i"
+        @click.prevent="setPage(i + pagerAddAmount)"
+        class="page-item"
+      >
+        <a class="page-link" href>{{ i + pagerAddAmount }}</a>
+      </li>
+
+      <li @click.prevent="setPage(currentPage + 1)" class="page-item">
+        <a class="page-link" href>Next</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script></script>
