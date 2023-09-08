8069608390eebd68521727d253eda3876604f347
Kuro Hsu <kurotanshi@gmail.com>
2023-09-08 14:51:33 +0800

    add Match Game

--- src/views/MatchGame/MatchGame.vue
+++ src/views/MatchGame/MatchGame.vue
@@ -1,63 +1,75 @@
 <script setup>
-import { ref } from 'vue';
+  import { ref, watch } from 'vue';
+  import flipCard from './components/flipCard.vue';
 
-// 試完成以下功能：
-//  1. 點擊卡片，卡片會翻開 (已完成)
-//  2. 點擊兩張不同的卡片，卡片會翻回去 
-//  3. 點擊兩張相同的卡片，卡片會消失
-//  4. 當所有卡片都消失時，顯示「恭喜破關，再來一局？」的對話框，按下確定後重置遊戲
-//  5. 將卡片獨立抽出為 Card.vue 元件
-
-const cards = ref([]);
-const openedCard = ref([]);
-
-// 遊戲初始化，洗牌
-const gameInit = () => {
-  const numArr = [...new Array(16).keys()].map(i => ++i);
-  numArr.sort(() => Math.random() - 0.5);
-  cards.value = numArr.map(d => (d % 8) + 1);
-  openedCard.value = [];
-}
-
-const clickHandler = (idx) => {    
-  openedCard.value.push(idx);
+  const cards = ref([]);
+  const openedCard = ref([]);
   
-  // 一秒後將 openedCard 清空 (牌面覆蓋回去)
-  window.setTimeout(() => {
+  const gameInit = () => {
+    const numArr = [...new Array(16).keys()].map(i => ++i);
+    numArr.sort(() => Math.random() - 0.5);
+    cards.value = numArr.map(d => (d % 8) + 1);
     openedCard.value = [];
-  }, 1000);
-}
+  }
+
+  const clickHandler = (idx) => {    
+    const i = openedCard.value.indexOf(idx);
+    const n = cards.value[idx];
+
+    if (openedCard.value.length < 2) {
+      if(i >= 0) {
+        openedCard.value.splice(i, 1);
+      }
+      else {
+        openedCard.value.push(idx);
+      }
+    }
+
+    // card match
+    if (openedCard.value.length === 2) {
+      if(cards.value[openedCard.value[0]] === cards.value[openedCard.value[1]]) {
+        window.setTimeout(() => {
+          cards.value = cards.value.map(d => (d === n) ? -1 : d);
+          openedCard.value = [];
+        }, 1000);
+      }
+      else {
+        window.setTimeout(() => {
+          openedCard.value = [];
+        }, 1000);
+      }
+    }
+  }
+
+  watch(cards, v => {
+    if(v.filter(d => d > 0).length === 0) {
+      confirm('恭喜破關，再來一局？') && gameInit();
+    }
+  });
 </script>
 
 <template>
   <div class="bg-emerald-900 h-[100vh] w-full top-0 left-0 z-10 absolute">
 
     <div class="my-10 text-white text-center ">
-      <div class="mb-8 text-5xl">五倍對對碰</div>
+      <h1 class="mb-8 text-5xl">五倍對對碰</h1>
       <button 
         @click="gameInit"
-        class="rounded font-bold bg-blue-500 mx-6 text-white py-2 px-4 hover:bg-blue-700">開始</button>
+        class="rounded font-bold bg-blue-500 mx-6 text-white py-2 px-4 hover:bg-blue-700">
+        {{ cards.length > 0 ? '重置' : '開始' }}
+      </button>
     </div>
 
-    <div class="rounded-xl mx-auto border-4 mt-12 grid grid-flow-col p-10 w-[900px] gap-2 grid-rows-4">
-      
-      <div 
-        v-for="(n, idx) in cards"
-        class="flip-card"
-        :class="{
-          'open': openedCard.includes(idx)
-        }"
-        @click="clickHandler(idx)">
-        <div class="flip-card-inner" v-if="cards[idx] > 0">
-          <div class="flip-card-front"></div>
-          <div class="flip-card-back">
-            <img :src="`./img/cat-0${n}.jpg`" alt="">
-          </div>
-        </div>
-      </div>
+    <div 
+      v-if="cards.length > 0"
+      class="rounded-xl mx-auto border-4 mt-12 grid grid-flow-col p-10 w-[900px] gap-2 grid-rows-4">
 
+      <flipCard 
+        v-for="(n, idx) in cards" 
+        :isOpened="openedCard.includes(idx)"
+        :isMatch="cards[idx] === -1"
+        :cardIdx="n"
+        @click="clickHandler(idx)" />
     </div>
   </div>
 </template>
-
-<style scoped src="./MatchGame.css"></style>
---
+++ src/views/MatchGame/components/flipCard.vue
@@ -0,0 +1,73 @@
+<script setup>
+const props = defineProps({
+  isOpened: {
+    type: Boolean,
+    default: false
+  },
+  isMatch: {
+    type: Boolean,
+    default: false
+  },
+  cardIdx: {
+    type: Number,
+    default: 1
+  },  
+});
+</script>
+
+<template>
+  <div 
+    class="flip-card" :class="{ 'open': isOpened }">
+    <div class="flip-card-inner" v-if="!isMatch">
+      <div class="flip-card-front">{{cardIdx}}</div>
+      <div class="flip-card-back">
+        <img :src="`/img/cat-0${cardIdx}.jpg`" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.flip-card {
+  @apply border-[#eee] border-2 h-[100px] m-4 w-[150px] block;
+}
+
+.flip-card-inner {
+  @apply h-full text-center w-full relative;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-card.open .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  @apply border-[#ddd] h-full border-2 w-full absolute;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  @apply bg-zinc-100;
+  /* background-color: #eee; */
+  background-image: url('./img/cat-bg.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position-x: 47.5%;
+  color: black;
+}
+
+.flip-card-back {      
+  background-color: #eee;
+  transform: rotateY(180deg);
+}
+
+.flip-card-back img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  background-color: #eee;
+}
+</style>
